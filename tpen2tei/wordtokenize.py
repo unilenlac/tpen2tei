2816ff7d0e070e92e9b481d4c4e8dcda02d02f35
Violeta Seretan <violeta.seretan@gmail.com>
2019-03-19 10:13:33 +0100

    avoid showing lb in 't'

--- tpen2tei/wordtokenize.py
+++ tpen2tei/wordtokenize.py
@@ -321,10 +321,10 @@ class Tokenizer:
         if len(tokens) > 1 and _is_blank(tokens[-1]):
             tokens.pop()
 
-        for t in tokens:
-            t['t'] = t['lit'] # VS: preserve XML around
-            # if 'join_next' not in t:
-            #     t['t'] += ' '  # VS: insert space for CollateX when running without tokeniser (-t)
+        if treatAsSingleton:
+            for t in tokens:
+                # preserve XML around
+                t['t'] = t['lit']
 
         return tokens
 
